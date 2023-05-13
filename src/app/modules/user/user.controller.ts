323298e2f17f5f8efa7c305ea781665ca8839bc3
emonShimoul <emonshimoul@gmail.com>
2023-05-13 16:52:37 +0600

    apply find, findOne, field filtering

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,5 +1,9 @@
 import { NextFunction, Request, Response } from "express";
-import { createUserToDB, getUsersFromDB } from "./user.service";
+import {
+  createUserToDB,
+  getUserByIdFromDB,
+  getUsersFromDB,
+} from "./user.service";
 
 export const createUser = async (
   req: Request,
@@ -28,5 +32,18 @@ export const getUsers = async (
   });
 };
 
+export const getUserById = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { id } = req.params;
+  const user = await getUserByIdFromDB(id);
+
+  res.status(200).json({
+    status: "success",
+    data: user,
+  });
+};
 // Pattern
 // Route -> controller -> service
